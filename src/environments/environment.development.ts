f35b2454c014012066594d70dc291cb3245d2254
SV <dalh072@gmail.com>
2025-09-21 16:12:29 +0300

    env added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,9 +3,9 @@ export const environment = {
   /**
    * Supabase project URL. Set this to the value from your Supabase project's settings panel.
    */
-  supabaseUrl: '',
+  supabaseUrl: 'https://demzdobotjkxqxkoqjrv.supabase.co',
   /**
    * Public anon key for the Supabase project. Never commit service role keys.
    */
-  supabaseAnonKey: '',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlbXpkb2JvdGpreHF4a29xanJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NTc5NzAsImV4cCI6MjA3NDAzMzk3MH0.NThghGWFp6TBAcLUxLQkgTi7JQE8av2_o8Inrzboadg',
 };
